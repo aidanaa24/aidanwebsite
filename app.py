be2480ac27d71f8a050f039ad40d299dfb17e514
aidanaa24 <155769109+aidanaa24@users.noreply.github.com>
2024-01-29 11:56:28 -0800

    initail commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+@app.route('/products')
+def products():
+    return render_template('products.html')
+if __name__ == '__main__':
+    app.run(debug=True)
